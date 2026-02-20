a5d6727845aa3ba86aee5114f1b248777f180abc
Ritvikesh253 <ritvikesh1234@gmail.com>
2026-02-20 10:02:27 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,26 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("Welcome to the Palindrome Checker App!");
+
+        // UC2: Hardcoded palindrome check
+        String hardcodedString = "radar";
+        if (isPalindrome(hardcodedString)) {
+            System.out.println(hardcodedString + " is a palindrome.");
+        } else {
+            System.out.println(hardcodedString + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
